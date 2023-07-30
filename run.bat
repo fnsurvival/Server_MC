3cf05a5ae7ca18d3cfd6d88858bdf1ec47e44dc9
papnineone <134521718+nanibrah@users.noreply.github.com>
2023-07-30 13:15:14 +0700

    Changed to my backup

--- run.bat
+++ run.bat
@@ -1,19 +1,9 @@
-title []
+title [FUNNY SURVIVAL]
 color
 @echo off
 :begin
 chcp 65001
 cls
-
-:: Change the number in "-Xms4G" and "-Xmx4G" to change ram usage and bandwidth, the higher the better
-:: Such as:
-:: + java -Xms5G -Xmx5G -Dfile.encoding=UTF-8 -jar server.jar
-:: + java -Xms10G -Xmx10G -Dfile.encoding=UTF-8 -jar server.jar
-:: + java -Xms15G -Xmx15G -Dfile.encoding=UTF-8 -jar server.jar
-:: + java -Xms25G -Xmx25G -Dfile.encoding=UTF-8 -jar server.jar
-:: You also can read more in "server.properties" file
-
-
 java -Xms4G -Xmx4G -Dfile.encoding=UTF-8 -jar server.jar
 timeout 5
 echo resuming server...
